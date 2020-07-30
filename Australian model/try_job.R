dfa4bf9a17f7ab188b2d2efe7c7920f3042064a0
szantamano <alexandrasouly@yahoo.fr>
2020-07-30 15:50:13 +0200

    trying on Tomis laptop

--- Australian model/try_job.R
+++ Australian model/try_job.R
@@ -7,7 +7,7 @@ library(ggplot2)
 library(ggpubr)
 
 library(doParallel)
-registerDoParallel(cores= 8)
+registerDoParallel(cores= 6)
 library(doRNG)
 registerDoRNG(625904618)
 library(pomp)
@@ -18,38 +18,44 @@ sobolDesign(
   nseq=20
 ) -> guesses
 
-Nmif = 30 
+Nmif = 1
 Np = 2000
-# filtering---
-  run_time <- system.time({
-        mf3<- foreach(guess=iter(guesses,"row"),
-                  .combine=c, 
+
+      mf3<- foreach(guess=iter(guesses,"row"),
+                .combine=c,
+                .packages = "pomp"
+
+      ) %dopar%
+      {
+        mif2(
+          pomp_object,
+          params = guess,
+          Nmif = Nmif,
+          Np = Np,
+          cooling.fraction.50=0.5,
+          cooling.type="geometric",
+          rw.sd=rw.sd(R0=0.02, rho_val=0.02, epsilon_val = 0.02, zeta=0.02,
+                      c=0.02, s=0.02, phi=0.02, s_v=0.02, phi_v=0.02, disp=0.02, d=0.01)
+        )
+      }
+
+save(Nmif, Np, guesses,  file = "Sili_filtering_1it.rda")
+
+mf3iter20<- foreach(mf3item=iter(mf3),
+                  .combine=c,
                   .packages = "pomp"
                   
-        ) %dopar%
-        {
-          mif2( 
-            pomp_object,
-            params = guess,
-            Nmif = Nmif, 
-            Np = Np, 
-            cooling.fraction.50=0.5,
-            cooling.type="geometric",
-            rw.sd=rw.sd(R0=0.02, rho_val=0.02, epsilon_val = 0.02, zeta=0.02,
-                        c=0.02, s=0.02, phi=0.02, s_v=0.02, phi_v=0.02, disp=0.02, d=0.01)
-          )
-        }
-  })
-  
-  save(Nmif, Np, run_time, guesses,  file = "Sili_filtering_proper.rda")
-  
-  mf3 %>%
-    traces() %>%
-    melt() %>%
-    filter(variable %in% c("loglik", "R0", "rho_val", "epsilon_val",  "zeta", "c",
-           "s", "phi","s_v", "phi_v", "disp", "d") )%>%
-    ggplot(aes(x=iteration,y=value,group=L1,color=L1))+
-    geom_line()+
-    facet_wrap(~variable,scales="free_y")+
-    guides(color=FALSE)
+            ) %dopar%
+  {
+              continue(mf3item, Nmif = 2)}
+
+mf3 %>%
+  traces() %>%
+  melt() %>%
+  filter(variable %in% c("loglik", "R0", "rho_val", "epsilon_val",  "zeta", "c",
+         "s", "phi","s_v", "phi_v", "disp", "d") )%>%
+  ggplot(aes(x=iteration,y=value,group=L1,color=L1))+
+  geom_line()+
+  facet_wrap(~variable,scales="free_y")+
+  guides(color=FALSE)
 

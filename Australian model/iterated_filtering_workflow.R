882f0f286bcbaf0266a6169f84fa91d840d0a8d4
szantamano <alexandrasouly@yahoo.fr>
2020-08-03 11:28:46 +0200

    reorganising folders

---
+++ Australian model/iterated_filtering_workflow.R
@@ -0,0 +1,82 @@
+source("full_pomp_model.R", local = TRUE)
+
+library(foreach)
+library(iterators)
+library(plyr)
+library(dplyr)
+library(ggplot2)
+library(ggpubr)
+library(doParallel)
+registerDoParallel(cores= 6)
+library(doRNG)
+registerDoRNG(625904618)
+library(pomp)
+# ----------------------------------------- params of the filtering --------------------------------
+# sobolDesign(
+#   lower=box[1,],
+#   upper=box[2,],
+#   nseq=10) -> guesses
+
+guesses <-  rbind(guesses, best_model_8, best_model_8, best_model_8, best_model_8, best_model_8)
+
+Nmif = 20
+Np = 4000
+# ------------------------------------------- filtering ---------------------------------------------
+      mf3<- foreach(guess=iter(guesses,"row"),
+                .combine=c,
+                .packages = "pomp"
+
+      ) %dopar%
+  
+      {   mif2(
+          pomp_object,
+          params = guess,
+          Nmif = Nmif,
+          Np = Np,
+          cooling.fraction.50=0.5,
+          cooling.type="geometric",
+          rw.sd=rw.sd(R0=0.02, zeta=0.02, omega_val = 0.02, gamma_val = 0.02,
+                      c=0.02, s=0.02, phi=0.02, s_v=0.02, phi_v=0.02, disp=0.02, d=0.01)
+        )
+      }
+
+      save(guesses, mf3,  file = "model8_filtering_top_pt1.rda")
+# ------------------------------------------- filtering part2 ---------------------------------------------
+      mf3iter2<- foreach(mf3item=iter(mf3),
+                        .combine=c,
+                        .packages = "pomp"
+      
+                  ) %dopar%
+                  {
+                    continue(mf3item, Nmif = 30)
+                    }
+
+      save(guesses, mf3iter2,  file = "model8_filtering_top_pt2.rda")
+
+# -------------- calculating the likelihoods properly ------------------------------------------------
+      
+      lik_list <- foreach(mf3item=iter(mf3iter2),
+                        .combine=rbind,
+                        .packages = "pomp"
+      
+                  ) %dopar%
+                  {
+                    evals <- replicate(5,logLik(pfilter(mf3item,Np=4000)))
+                    ll <- logmeanexp(evals,se=TRUE)
+                    c(coef(mf3item),loglik=ll[1],loglik=ll[2])
+                  }
+      
+      lik_list <- as.data.frame(lik_list)
+      # #staring a new csv file 
+      lik_list %>%
+      arrange(-loglik) %>%
+      write.csv("model8_likelihoods_top_again.csv")
+      # 
+      # 
+      # # #adding in more points to my csv of likelihoods
+      # read.table("model4_likelihoods.csv",sep = ",", row.names = 1, header = TRUE) %>%
+      # bind_rows(lik_list) %>%
+      # arrange(-loglik) %>% write.csv("model4_likelihoods.csv")
+
+
+
--- Australian model/load_data.R
+++ Australian model/load_data.R
@@ -3,13 +3,13 @@ load_data <- function(site, year){
   # loading observed data table from chosen year, filtering by site 
   
 if (as.character(year) =="all"){
-  file_name <- paste("prev_data_by_sites_2017.txt")
+  file_name <- paste("data/prev_data_by_sites_2017.txt")
   data_2017<-read.table(as.character(file_name))
   
-  file_name <- paste("prev_data_by_sites_2018.txt")
+  file_name <- paste("data/prev_data_by_sites_2018.txt")
   data_2018<-read.table(as.character(file_name))
   
-  file_name <- paste("prev_data_by_sites_2019.txt")
+  file_name <- paste("data/prev_data_by_sites_2019.txt")
   data_2019<-read.table(as.character(file_name))
   
   merge(merge(data_2017, data_2018, all = TRUE), data_2019, all = TRUE) -> data_all_year
@@ -36,7 +36,7 @@ if (as.character(year) =="all"){
   
 }else {
   
-  file_name <- paste("prev_data_by_sites_", year, ".txt", sep = "")
+  file_name <- paste("data/prev_data_by_sites_", year, ".txt", sep = "")
   data_2019<-read.table(as.character(file_name))
   data_2019 %>% 
     filter(site_abbrev == site) %>% 
